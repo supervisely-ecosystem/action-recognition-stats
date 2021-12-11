05af4c4871aacd2fe46d6d3e852f361a9aed5ebe
Daniil Yarockii <yaroc.d@gmail.com>
2021-12-11 14:33:50 +0300

    pie-charts in progress

---
+++ src/sly_constants.py
@@ -0,0 +1,18 @@
+
+solo_button_stages = {
+    0: {
+        'stage': 0,  # disabled
+        'soloButtonColorText': '#3d3d3d',
+        'soloButtonColorBg': 'white'
+    },
+    1: {
+        'stage': 1,  # tagged frames
+        'soloButtonColorText': 'white',
+        'soloButtonColorBg': '#f8ba29'
+    },
+    2: {
+        'stage': 2,  # untagged frames
+        'soloButtonColorText': '#f8ba29',
+        'soloButtonColorBg': 'white'
+    }
+}
